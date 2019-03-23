0fec5445ae31f7445efd6d167d9109f39b4c6ead
veip007 <12257917+veip007@users.noreply.github.com>
2019-03-23 15:58:08 +0800

    Update Network-Reinstall-System-Modify.sh

--- Network-Reinstall-System-Modify.sh
+++ Network-Reinstall-System-Modify.sh
@@ -21,7 +21,7 @@ echo -e "\n\n\n"
 clear
 echo -e "\n"
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[33m Network-Reinstall-System-Modify Tools V2.0.1 2019/01/06 \033[0m"
+echo -e "\033[33m Network-Reinstall-System-Modify Tools V2.1.0 2019/03/22 \033[0m"
 echo -e "\033[33m [Magic Modify] Reinstall the system (any Windows / Linux) requires only network and one click \033[0m"
 echo -e "\033[33m System requirements: Any Linux system with GRUB or GRUB2, recommended CentOS7/Debian9/Ubuntu18.04 \033[0m"
 echo -e "\n"
@@ -37,7 +37,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest CentOS_7 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://doc-0g-08-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/2ll3u65bacqm7gqn9ukj9hk0f45u3958/1548338400000/17128039988164006870/*/1mCRpOOK6CKoJxRbunp8mlKIPvA2ukMqQ?e=download'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://www.kiss95.win/DD/MeowLove/CentOS_7.X_NetInstallation.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-CentOS_6' ]
@@ -45,7 +45,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest CentOS_6 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -c 6.9 -v 64 -a
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -c 6.9 -v 64 -a --mirror 'http://mirror.centos.org/centos'
 fi
 
 if [ $1 = '-Debian_9' ]
@@ -53,7 +53,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Debian_9 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -d 9 -v 64 -a
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -d 9 -v 64 -a --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Debian_8' ]
@@ -61,7 +61,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Debian_8 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -d 8 -v 64 -a
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -d 8 -v 64 -a --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Debian_7' ]
@@ -69,7 +69,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Debian_7 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -d 7 -v 64 -a
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -d 7 -v 64 -a -a --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Ubuntu_18.04' ]
@@ -77,7 +77,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Ubuntu_18.04 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -u 18.04 -v 64 -a
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -u 18.04 -v 64 -a -a --mirror 'http://archive.ubuntu.com/ubuntu'
 fi
 
 if [ $1 = '-Ubuntu_16.04' ]
@@ -85,7 +85,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Ubuntu_16.04 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -u 16.04 -v 64 -a
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -u 16.04 -v 64 -a -a --mirror 'http://archive.ubuntu.com/ubuntu'
 fi
 
 if [ $1 = '-Ubuntu_14.04' ]
@@ -93,7 +93,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Ubuntu_14.04 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -u 14.04 -v 64 -a
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -u 14.04 -v 64 -a -a --mirror 'http://archive.ubuntu.com/ubuntu'
 fi
 
 if [ $1 = '-Windows_Server_2019' ]
@@ -101,7 +101,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Windows_Server_2019 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://doc-0g-08-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/n29qhlft4urki81tup15ta1ru60dlqfm/1548338400000/17128039988164006870/*/1XdwTBt1k4ykYzTXJLlCw3ST3DgT6nZbS?e=download'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://www.kiss95.win/DD/MeowLove/Disk_Windows_Server_2019_DataCenter_CN.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Windows_Server_2016' ]
@@ -109,7 +109,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Windows_Server_2016 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://doc-0o-08-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/pfgbbt7ojhu3fk95b70k5ehivp3nnf2v/1548338400000/17128039988164006870/*/1vrMnQImTf9qPIN3kqclVu9bBeRwPr1lG?e=download'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://www.kiss95.win/DD/MeowLove/Disk_Windows_Server_2016_DataCenter_CN.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Windows_Server_2012R2' ]
@@ -117,7 +117,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Windows_Server_2012R2 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://doc-10-08-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/j3oi6c1d6bfuc0ai8e2ll59lsi9ami3r/1548338400000/17128039988164006870/*/1d5vGH37m0wD60GhmkuOIRj_ZJXGml9vo?e=download'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://www.kiss95.win/DD/MeowLove/Disk_Windows_Server_2012R2_DataCenter_CN.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Windows_Server_2008R2' ]
@@ -125,7 +125,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Windows_Server_2008R2 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://doc-08-08-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/21dnv7019ea0vusei94s8qh1qv7h8hek/1548338400000/17128039988164006870/*/1E8wAdPMCFOBHGtlFAVmfJVF3OD1UpVp4?e=download'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://www.kiss95.win/DD/MeowLove/Disk_Windows_Server_2008R2_DataCenter_CN.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Windows_7_Vienna' ]
@@ -133,7 +133,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Windows_7_Vienna \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://doc-0c-08-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/ej5rasa02adse4lb8o1g7d3hnpmpj29p/1548338400000/17128039988164006870/*/104tcg6ZiXFiHux7Z-9gGoWh_U2aPsrGj?e=download'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://www.kiss95.win/DD/MeowLove/Disk_Windows_7_Vienna_Ultimate_CN.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-Windows_Server_2003' ]
@@ -141,7 +141,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest Windows_Server_2003 \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://doc-00-08-docs.googleusercontent.com/docs/securesc/ha0ro937gcuc7l7deffksulhg5h7mbp1/h0qtgalikkta9ahs86fa7p6lctv4sjvl/1548338400000/17128039988164006870/*/13G6T-rRzlFjWcCua56p3n_apvwDQINQh?e=download'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://www.kiss95.win/DD/MeowLove/Disk_Windows_Server_2003_DataCenter_CN.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-CXT_Bare-metal_System_Deployment_Platform' ]
@@ -149,7 +149,7 @@ then
 	echo -e "\033[33m You have chosen to install the latest CXT_Bare-metal_System_Deployment_Platform \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreFiles/CXT_Bare-metal_System_Deployment_Platform.vhd.gz'
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreFiles/CXT_Bare-metal_System_Deployment_Platform.vhd.gz' --mirror 'http://deb.debian.org/debian'
 fi
 
 if [ $1 = '-DD' ]
@@ -157,14 +157,12 @@ then
 	echo -e "\033[33m You have chosen to install the DD package provided by you \033[0m"
 	echo -e "\n"
 	sleep 2s
-	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd $2
+	wget --no-check-certificate -qO koiok.sh 'https://raw.githubusercontent.com/veip007/Network-Reinstall-System-Modify/master/CoreShell/koiok.sh' && bash koiok.sh -dd $2 --mirror 'http://deb.debian.org/debian'
 fi
 
 
 echo "---------------------------------------------------------------------------------------------------------------------"
-echo -e "\033[31m !!!!!!!!Error!!!!!!!!Error!!!!!!!!Error!!!!!!!! \033[0m"
-echo -e "\033[31m Please check if the options are correct. For example [-CXT_Bare-metal_System_Deployment_Platform/-CentOS_7/-Windows_Server_2019]/-DD '%URL' \033[0m"
+echo -e "\033[33m Enjoy It ! \033[0m"
 echo "---------------------------------------------------------------------------------------------------------------------"
 echo -e "\n"
 exit
-
